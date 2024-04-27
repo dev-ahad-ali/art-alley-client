3d185782ddcb222b9c7e3db9e2adafee5fce5164
dev-ahad-ali <ahadali.dev@gmail.com>
2024-04-27 14:12:30 +0600

    react tost added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Root = () => {
     return (
@@ -8,6 +10,18 @@ const Root = () => {
                 <Navbar />
             </header>
             <Outlet />
+            <ToastContainer
+                position='top-center'
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                transition={Zoom}
+            />
         </>
     );
 };
