4f0752bec6a2db2981466680255441d28fae13b7
dev-ahad-ali <ahadali.dev@gmail.com>
2024-04-29 22:54:28 +0600

    registration page style added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -29,8 +29,7 @@ const Login = () => {
                 if (result.user) {
                     toast.success('Login successfully');
                     if (result.user) {
-                        // navigate(from);
-                        console.log(result.user);
+                        navigate(from);
                     }
                 }
             })
@@ -96,37 +95,40 @@ const Login = () => {
                         </p>
                     )}
                 </div>
-                <div className='relative'>
-                    <label className='input input-bordered flex items-center gap-2'>
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 16 16'
-                            fill='currentColor'
-                            className='h-4 w-4 opacity-70'
-                        >
-                            <path
-                                fillRule='evenodd'
-                                d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-                                clipRule='evenodd'
+                <div>
+                    <div className='relative'>
+                        <label className='input input-bordered flex items-center gap-2'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                viewBox='0 0 16 16'
+                                fill='currentColor'
+                                className='h-4 w-4 opacity-70'
+                            >
+                                <path
+                                    fillRule='evenodd'
+                                    d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
+                                    clipRule='evenodd'
+                                />
+                            </svg>
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                className='grow'
+                                placeholder='Password'
+                                {...register('Password', {
+                                    required: {
+                                        value: true,
+                                        message: 'Must enter a password',
+                                    },
+                                })}
                             />
-                        </svg>
-                        <input
-                            type={showPassword ? 'text' : 'password'}
-                            className='grow'
-                            {...register('Password', {
-                                required: {
-                                    value: true,
-                                    message: 'Must enter a password',
-                                },
-                            })}
-                        />
-                    </label>
-                    <span
-                        className='absolute right-2 top-1/2 -translate-y-1/2 text-xl'
-                        onClick={handleShow}
-                    >
-                        {showPassword ? <FaEye /> : <FaEyeSlash />}
-                    </span>
+                        </label>
+                        <span
+                            className='absolute right-2 top-1/2 -translate-y-1/2 text-xl'
+                            onClick={handleShow}
+                        >
+                            {showPassword ? <FaEye /> : <FaEyeSlash />}
+                        </span>
+                    </div>
                     {errors.Password ? (
                         <p className='font-noto text-sm font-semibold text-red-500'>
                             {errors.Password.message}
