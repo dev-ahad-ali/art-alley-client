21c4c3d15215379a468ce8ed1ecd731167f59aa6
dev-ahad-ali <ahadali.dev@gmail.com>
2024-04-30 01:40:02 +0600

    deploy fixed

--- src/Utils/Url.js
+++ src/Utils/Url.js
@@ -1,3 +1,2 @@
-// export const url =
-//     'https://art-alley-server-ittijfqx2-ahad-alis-projects-3b3af95c.vercel.app';
-export const url = 'http://localhost:5000';
+export const url = 'https://art-alley-server.vercel.app';
+// export const url = 'http://localhost:5000';
