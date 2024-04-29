9aa42fefc96bf161016ffeaa087e1e35d0fb7c97
dev-ahad-ali <ahadali.dev@gmail.com>
2024-04-30 00:28:11 +0600

    sub category page style added

--- src/Pages/SubCategory.jsx
+++ src/Pages/SubCategory.jsx
@@ -1,15 +1,23 @@
 import { useLoaderData, useLocation } from 'react-router-dom';
+import ExhibitionCard from '../components/Cards/ExhibitionCard';
 
 const SubCategory = () => {
     const location = useLocation();
     const data = useLoaderData();
     const categories = data.data;
     return (
-        <div>
-            <h2 className='text-2xl'>sub category : {location.state}</h2>
-            {categories.map((category) => (
-                <p key={category._id}>{category.itemName}</p>
-            ))}
+        <div className='mx-auto max-w-7xl px-4'>
+            <h2 className='mt-6 text-center font-yeseva text-[60px]'>
+                Category :{' '}
+                <span className='text-amber-800 underline'>
+                    {location.state}
+                </span>
+            </h2>
+            <div className='mt-8 grid grid-cols-3 gap-x-4 gap-y-8'>
+                {categories.map((art) => (
+                    <ExhibitionCard key={art._id} art={art} />
+                ))}
+            </div>
         </div>
     );
 };
