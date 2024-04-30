c514ce9fb686a69f44fd913b09b00e0c823291b0
dev-ahad-ali <ahadali.dev@gmail.com>
2024-04-30 09:15:42 +0600

    error page added

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,8 +1,32 @@
+import { BiChevronsLeft, BiSolidMessageSquareError } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+
 const Error = () => {
     return (
-        <div>
-            <h2 className='text-2x'>this is error page</h2>
-        </div>
+        <section className='grid min-h-screen w-full place-items-center bg-gray-500'>
+            <div className='text-center'>
+                <div className='flex justify-center'>
+                    <BiSolidMessageSquareError className='text-brown-600 text-[300px]' />
+                </div>
+                <h2 className='font-lobster text-[100px] leading-[100px] opacity-60'>
+                    404
+                </h2>
+                <p className='-mt-4 font-yeseva text-[40px] opacity-65'>
+                    Page Not Found
+                </p>
+                <div className='absolute left-5 top-5'>
+                    <Link
+                        to={'/'}
+                        className='group flex items-center gap-1 p-4'
+                    >
+                        <BiChevronsLeft className='relative text-[40px] text-amber-500 duration-300 group-hover:-translate-x-2' />
+                        <span className='font-noto text-2xl font-light text-amber-500'>
+                            Back Home
+                        </span>
+                    </Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/Utils/Url.js
+++ src/Utils/Url.js
@@ -1,2 +1,2 @@
-export const url = 'https://art-alley-server.vercel.app';
-// export const url = 'http://localhost:5000';
+// export const url = 'https://art-alley-server.vercel.app';
+export const url = 'http://localhost:5000';
